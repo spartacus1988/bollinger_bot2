6bbbf7907d252c53d87e0a6e692493b2c9418054
mixxxxx <spartacusxxxxxx@gmail.com>
2017-12-13 17:17:08 +0300

    fucking old bugs

--- bollinger_bot2.py
+++ bollinger_bot2.py
@@ -63,9 +63,9 @@ def send_handler(api, math, mail, config, cryptocurrency, rate):
         token = config['token']
 
         if (config['mode'] == 'PROD'):
-            chat_id = config['users'][4]
+            chat_id = config['users'][2]
         else:
-            chat_id = config['users'][4]
+            chat_id = config['users'][2]
 
         telegram = bb_telegram.bb_telegram(token, chat_id)
 
@@ -95,7 +95,7 @@ def main():
         if first_one: api = bb_api.bb_api(); math = bb_math.bb_math(); mail = bb_mail.bb_mail()
 
         #get all needed cryptocurrency
-        #api.merged_currencies = api.check_all_coins('all_coins.txt', config['min_Cap'], config['24hr_Vol'])
+        api.merged_currencies = api.check_all_coins('all_coins.txt', config['min_Cap'], config['24hr_Vol'])
 
 
         #cryptocurrency = 'BTC'
@@ -105,92 +105,92 @@ def main():
                 #get SortedDict({time:price}) from time_before
                 api.build_url_crypto_compare(cryptocurrency, 'BTC', str(int(time_before)), config['limit'])
                 #print(api.url_crypto_compare)
-            #else:
-        api.build_url_crypto_compare('BTC', 'USD', str(int(time_before)), config['limit'])
+            else:
+                api.build_url_crypto_compare('BTC', 'USD', str(int(time_before)), config['limit'])
                 #print(api.url_crypto_compare)
 
 
 
-        api.json_crypto_compare = api.request(api.url_crypto_compare)
-        prices = api.extract_crypto_compare()
+            api.json_crypto_compare = api.request(api.url_crypto_compare)
+            prices = api.extract_crypto_compare()
 
-        typical_prices = api.extract_typical_prices()
-        #print(typical_prices)
+            typical_prices = api.extract_typical_prices()
+            #print(typical_prices)
 
-        MF_dict = api.MF_extract()
-        #print(MF_dict)
-
-
-
-            #print(prices)
-
-        if len(prices.keys()) != 0 and len(MF_dict.keys()) != 0:
+            MF_dict = api.MF_extract()
+            #print(MF_dict)
 
 
 
-            #get input_dict for math
-            math.input_dict = prices
-            math.typical_prices = typical_prices
-            math.MF_dict = MF_dict
+                #print(prices)
 
-            #calculating MFI
-            math.MFI = math.MFI_calc()
-
-            #calculating MACD
-            math.exp_mov_avg_20 = math.exp_moving_average_dict(math.input_dict, 20)
-            print('exp_mov_avg_20 is ' + str(math.exp_mov_avg_20[0]))
-
-            math.exp_mov_avg_10 = math.exp_moving_average_dict(math.input_dict, 10)
-            print('exp_mov_avg_10 is ' + str(math.exp_mov_avg_10[0]))
-
-            math.MACD = math.exp_mov_avg_10[0] - math.exp_mov_avg_20[0]
-            print('MACD is ' + str(math.MACD))
-
-            if math.MACD_prev is None:
-                math.MACD_prev = math.MACD
-                math.MACD_delta = 0
-            else:
-                math.MACD_delta = math.MACD - math.MACD_prev
-                math.MACD_prev = math.MACD
-            print('MACD_delta is ' + str(math.MACD_delta))
+            if len(prices.keys()) != 0 and len(MF_dict.keys()) != 0:
 
 
 
-            if ((config['use_MFI']) and ((math.MFI > 80) or (math.MFI < 20))) or not config['use_MFI']:
+                #get input_dict for math
+                math.input_dict = prices
+                math.typical_prices = typical_prices
+                math.MF_dict = MF_dict
 
-                print("MFI condition is ok")
-                print("math.MFI is " + str(math.MFI))
-                #calculating mov_avg
-                math.running_avg = math.moving_average_FOUR(math.input_dict, config['num_avg'])
+                #calculating MFI
+                math.MFI = math.MFI_calc()
 
-                #calculating std
-                math.std_value = math.bb_std(math.input_dict)
-                print('math.std_value for ' + cryptocurrency + ' is %f' % math.std_value)
-                #print(math.std_value)
-                sys.stdout.flush()
+                #calculating MACD
+                math.exp_mov_avg_20 = math.exp_moving_average_dict(math.input_dict, 20)
+                print('exp_mov_avg_20 is ' + str(math.exp_mov_avg_20[0]))
 
-                #calculating upper_line
-                #print(math.running_avg)
-                math.upper_line = math.bb_upper_line()
+                math.exp_mov_avg_10 = math.exp_moving_average_dict(math.input_dict, 10)
+                print('exp_mov_avg_10 is ' + str(math.exp_mov_avg_10[0]))
 
-                #calculating lower_line
-                math.lower_line = math.bb_lower_line()
+                math.MACD = math.exp_mov_avg_10[0] - math.exp_mov_avg_20[0]
+                print('MACD is ' + str(math.MACD))
+
+                if math.MACD_prev is None:
+                    math.MACD_prev = math.MACD
+                    math.MACD_delta = 0
+                else:
+                    math.MACD_delta = math.MACD - math.MACD_prev
+                    math.MACD_prev = math.MACD
+                print('MACD_delta is ' + str(math.MACD_delta))
 
 
-                #print("debug")
-                #send_handler(api, math, mail, config, cryptocurrency, 'debug')
 
-                #SIGNAL to BUY
-                if (math.bb_compare_to_buy(math.input_dict.values()[-1:][0], math.lower_line.values()[-1:][0],math.upper_line.values()[-1:][0], config['percent'])) and (math.std_value > 0) and (math.MFI < 20 or not config['use_MFI']):
-                    if ((config['use_MACD']) and (math.MACD_delta > 0)) or not config['use_MACD']:
-                        print("BUY " + cryptocurrency)
-                        send_handler(api, math, mail, config, cryptocurrency, 'buying')
+                if ((config['use_MFI']) and ((math.MFI > 80) or (math.MFI < 20))) or not config['use_MFI']:
 
-                #SIGNAL to SELL
-                if (math.bb_compare_to_sell(math.input_dict.values()[-1:][0], math.lower_line.values()[-1:][0],math.upper_line.values()[-1:][0], config['percent'])) and (math.std_value > 0) and (math.MFI > 80 or not config['use_MFI']):
-                    if ((config['use_MACD']) and (math.MACD_delta < 0)) or not config['use_MACD']:
-                        print("SELL " + cryptocurrency)
-                        send_handler(api, math, mail, config, cryptocurrency, 'selling')
+                    print("MFI condition is ok")
+                    print("math.MFI is " + str(math.MFI))
+                    #calculating mov_avg
+                    math.running_avg = math.moving_average_FOUR(math.input_dict, config['num_avg'])
+
+                    #calculating std
+                    math.std_value = math.bb_std(math.input_dict)
+                    print('math.std_value for ' + cryptocurrency + ' is %f' % math.std_value)
+                    #print(math.std_value)
+                    sys.stdout.flush()
+
+                    #calculating upper_line
+                    #print(math.running_avg)
+                    math.upper_line = math.bb_upper_line()
+
+                    #calculating lower_line
+                    math.lower_line = math.bb_lower_line()
+
+
+                    #print("debug")
+                    #send_handler(api, math, mail, config, cryptocurrency, 'debug')
+
+                    #SIGNAL to BUY
+                    if (math.bb_compare_to_buy(math.input_dict.values()[-1:][0], math.lower_line.values()[-1:][0],math.upper_line.values()[-1:][0], config['percent'])) and (math.std_value > 0) and (math.MFI < 20 or not config['use_MFI']):
+                        if ((config['use_MACD']) and (math.MACD_delta > 0)) or not config['use_MACD']:
+                            print("BUY " + cryptocurrency)
+                            send_handler(api, math, mail, config, cryptocurrency, 'buying')
+
+                    #SIGNAL to SELL
+                    if (math.bb_compare_to_sell(math.input_dict.values()[-1:][0], math.lower_line.values()[-1:][0],math.upper_line.values()[-1:][0], config['percent'])) and (math.std_value > 0) and (math.MFI > 80 or not config['use_MFI']):
+                        if ((config['use_MACD']) and (math.MACD_delta < 0)) or not config['use_MACD']:
+                            print("SELL " + cryptocurrency)
+                            send_handler(api, math, mail, config, cryptocurrency, 'selling')
 
 
         #delay for 5 minutes
